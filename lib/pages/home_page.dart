141f740df2b40fd6cde98144a3088aac0126fd76
Cesar Morales <cesarmorales@cardioperfusion.com>
2023-05-19 17:31:56 -0500

    Acabando drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,7 +8,87 @@ class HomPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Text("Hola"),
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                        "https://images.pexels.com/photos/13813545/pexels-photo-13813545.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+                  ),
+                ),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.white12,
+                        backgroundImage: NetworkImage(
+                          "https://images.pexels.com/photos/8062069/pexels-photo-8062069.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                        ),
+                      ),
+                      Text(
+                        "Fiorella Gonzales Tapia",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "Administrador",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text("Mi perfil"),
+                leading: Icon(
+                  Icons.people,
+                  color: Colors.black.withOpacity(0.6),
+                ),
+              ),
+              ListTile(
+                title: Text("Portafolio General"),
+                leading: Icon(
+                  Icons.file_copy,
+                  color: Colors.black.withOpacity(0.6),
+                ),
+              ),
+              ListTile(
+                title: Text("Cambiar contraseña"),
+                leading: Icon(
+                  Icons.lock,
+                  color: Colors.black.withOpacity(0.6),
+                ),
+              ),
+              Divider(
+                indent: 12,
+                endIndent: 12,
+              ),
+              ListTile(
+                title: Text("Salir del App"),
+                leading: Icon(
+                  Icons.exit_to_app,
+                  color: Colors.black.withOpacity(0.6),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
         title: Text("Shareds Preferences App"),
       ),
